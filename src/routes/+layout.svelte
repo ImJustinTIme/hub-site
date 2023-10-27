b1a09925a2a547d517ef1bd73a78f161f5532eca
ImJustinTIme <49542450+ImJustinTIme@users.noreply.github.com>
2023-10-26 19:02:03 -0500

    wip

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<slot />
+<footer>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/settings">Settings</a>
+</footer>
+
+<style type="scss">
+  footer {
+    padding: 20px 20px 15px 20px;
+    background-color: var(--accent);
+    height: auto;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<div class="content">
+  <div class="titlebar">
+    <span class="title">codejustin.dev</span>
+    <div class="extra-info" />
+  </div>
+  <div class="interactive-map" />
+</div>
+
+<style type="scss">
+  .content {
+    width: inherit;
+  }
+
+  .interactive-map {
+    border-radius: 30px;
+    margin: 20px 20px 40px 20px;
+    background: var(--primary);
+    height: 80vh;
+  }
+
+  .titlebar {
+    margin: 0 40px;
+  }
+
+  .title {
+    background: linear-gradient(120deg, var(--primary), var(--accent));
+    background-clip: text;
+    font-size: 4rem;
+    font-weight: 600;
+    fill: transparent;
+  }
+</style>
