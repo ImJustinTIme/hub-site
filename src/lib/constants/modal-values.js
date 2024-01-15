21fca926a19d1f8a1dd0d01398e5754cab659bd4
ImJustinTIme <jukuennen@gmail.com>
2024-01-15 12:30:57 -0600

    Update modal-values.js

--- src/lib/constants/modal-values.js
+++ src/lib/constants/modal-values.js
@@ -31,11 +31,15 @@ export const gameCornerContentArr = [
 
 export const projectsPageContentArr = [
     {
-        appLink: 'https://imjustintime.github.io/asteroids.js/',
-        alt: 'AsteroidsJs Screenshot',
-        imgSrc: '/AsteroidsJs.png',
-        descriptionText: "A web version of the classic game asteroids written in vanilla javascript with the ability to save local highscores.",
-        srcLink: 'https://github.com/ImJustinTIme/asteroids.js',
-        title: 'AsteroidsJs'
+        alt: 'Tetris Clock',
+        descriptionText: 'A work in progress clock made with a Matrix Portal M4 and a dot matrix display on it. The idea is to have tetriminos fall down and form the numbers displaying the time.',
+        srcLink: 'https://github.com/ImJustinTIme/tetris-clock',
+        title: 'Tetris Clock'
     },
+    {
+        title: 'Kitchen Korner',
+        alt: 'Kitchen Korner',
+        descriptionText: 'Android app build at Iowa State University for COMS 311. It lets the user save and search for recipes from a variety of online apis. Writen in Kotlin',
+        srcLink: 'https://github.com/ImJustinTIme/kitchenkorner'
+    }
 ];
