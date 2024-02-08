287394519f6bb2b7fd6684ca8ccc42f4705393a6
ImJustinTIme <jukuennen@gmail.com>
2024-02-07 21:13:31 -0600

    lots of little changes

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.tjWEV5WH.js","_app/immutable/chunks/scheduler.H_AjIzbe.js","_app/immutable/chunks/index.Ngrm9XW4.js","_app/immutable/chunks/entry.2ypGetwx.js","_app/immutable/chunks/index.969uwfpS.js","_app/immutable/chunks/control.pJ1mnnAb.js"];
+export const imports = ["_app/immutable/nodes/1.8ATXCBBI.js","_app/immutable/chunks/scheduler.H_AjIzbe.js","_app/immutable/chunks/index.Ngrm9XW4.js","_app/immutable/chunks/entry.puUpeeSq.js","_app/immutable/chunks/index.969uwfpS.js","_app/immutable/chunks/control.pJ1mnnAb.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set([".DS_Store","AsteroidsJs.png","Crunchy (Small) 2.png","Crunchy (Small).png","Crunchy (Small).svg","close.svg","justink.jpg"]),
+	assets: new Set([".DS_Store","AsteroidsJs.png","Crunchy (Small) 2.png","Crunchy (Small).png","Crunchy (Small).svg","KitchenKorner.png","Onlink.png","close.svg","justink.jpg"]),
 	mimeTypes: {".png":"image/png",".svg":"image/svg+xml",".jpg":"image/jpeg"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.v-gPbn2e.js","app":"_app/immutable/entry/app.qN11YiOl.js","imports":["_app/immutable/entry/start.v-gPbn2e.js","_app/immutable/chunks/entry.2ypGetwx.js","_app/immutable/chunks/scheduler.H_AjIzbe.js","_app/immutable/chunks/index.969uwfpS.js","_app/immutable/chunks/control.pJ1mnnAb.js","_app/immutable/entry/app.qN11YiOl.js","_app/immutable/chunks/scheduler.H_AjIzbe.js","_app/immutable/chunks/index.Ngrm9XW4.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.JTeJ6f9k.js","app":"_app/immutable/entry/app.EfcP9O4a.js","imports":["_app/immutable/entry/start.JTeJ6f9k.js","_app/immutable/chunks/entry.puUpeeSq.js","_app/immutable/chunks/scheduler.H_AjIzbe.js","_app/immutable/chunks/index.969uwfpS.js","_app/immutable/chunks/control.pJ1mnnAb.js","_app/immutable/entry/app.EfcP9O4a.js","_app/immutable/chunks/scheduler.H_AjIzbe.js","_app/immutable/chunks/index.Ngrm9XW4.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
